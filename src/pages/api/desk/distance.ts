c681fc5b86b32668b5a78b4b4516a97f866f4ee8
Asher <jasherleslie@gmail.com>
2023-06-22 16:04:02 +1000

    Cors things.

--- src/pages/api/desk/distance.ts
+++ src/pages/api/desk/distance.ts
@@ -1,23 +1,42 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
 import { prisma } from "../../../server/db";
+import Cors from "cors";
 
 interface AddDistanceBody {
   distance: number;
+}
+
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+// eslint-disable-next-line
+const cors = Cors({ methods: ["POST"] });
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse) {
+  return new Promise((resolve, reject) => {
+    cors(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
 }
 
 const addDistanceHandler = async (
   req: NextApiRequest,
   res: NextApiResponse
 ) => {
+  await runMiddleware(req, res);
+
   if (req.method !== "POST") {
     return res.status(405).end();
   }
 
-  console.log(req.body);
-  console.log(typeof req.body);
-
   // eslint-disable-next-line
-  const data: AddDistanceBody = JSON.parse(req.body);
+  const data: AddDistanceBody = req.body;
 
   if (data.distance === undefined || typeof data.distance !== "number") {
     return res.status(400).json({ error: "Invalid distance" });
@@ -27,7 +46,11 @@ const addDistanceHandler = async (
     data: { distance: data.distance },
   });
 
-  res.status(200).json({ success: true, ...deskDistance });
+  res
+    .status(200)
+    .setHeader("Access-Control-Allow-Origin", "*")
+    .setHeader("Access-Control-Allow-Methods", "POST")
+    .json({ success: true, ...deskDistance });
 };
 
 export default addDistanceHandler;
