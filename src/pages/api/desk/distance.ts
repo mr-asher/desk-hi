8a23168b6407cff875138f6efebdf3aa7e7647c1
Asher <jasherleslie@gmail.com>
2023-06-22 11:07:14 +1000

    Can post to server with desk height.

---
+++ src/pages/api/desk/distance.ts
@@ -0,0 +1,25 @@
+import { type NextApiRequest, type NextApiResponse } from "next";
+import { prisma } from "../../../server/db";
+
+const addDistanceHandler = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  if (req.method !== "POST") {
+    return res.status(405).end();
+  }
+
+  const data = JSON.parse(req.body);
+
+  if (data.distance === undefined || typeof data.distance !== "number") {
+    return res.status(400).json({ error: "Invalid distance" });
+  }
+
+  const deskDistance = await prisma.deskDistance.create({
+    data: { distance: data.distance },
+  });
+
+  res.status(200).json({ success: true, ...deskDistance });
+};
+
+export default addDistanceHandler;
